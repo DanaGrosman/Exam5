9e7c98cf942ecae0693160449e6247c514e76d43
DanaGrosman <grosmandanna@gmail.com>
2022-05-19 15:34:30 +0300

    Q1 and Q2

--- src/ajbc/exam5/q1/classes/BadEnemyToEnemyAdapter.java
+++ src/ajbc/exam5/q1/classes/BadEnemyToEnemyAdapter.java
@@ -5,42 +5,43 @@ import ajbc.exam5.q1.interfaces.Enemy;
 
 public class BadEnemyToEnemyAdapter implements Enemy {
 
-	BadEnemy badEnemy;
+	private BadEnemy badEnemy;
+	private String badEnemyName;
 
 	public BadEnemyToEnemyAdapter(BadEnemy badEnemy) {
 		this.badEnemy = badEnemy;
+		badEnemyName = badEnemy.getClass().getSimpleName();
 	}
 
 	@Override
 	public void turnRight() {
-		System.out.print("This may be " + badEnemy.getClass().getSimpleName() + " but its a Enemy that turn right: ");
+		System.out.print("This may be " + badEnemyName + " but its a Enemy that turn right: ");
 		badEnemy.yawsRight();
 	}
 
 	@Override
 	public void turnLeft() {
-		System.out.print("This may be " + badEnemy.getClass().getSimpleName() + " but its a Enemy that turn left: ");
+		System.out.print("This may be " + badEnemyName + " but its a Enemy that turn left: ");
 		badEnemy.yawsLeft();
 	}
 
 	@Override
 	public void driveForward() {
-		System.out
-				.print("This may be " + badEnemy.getClass().getSimpleName() + " but its a Enemy the drive forword: ");
+		System.out.print("This may be " + badEnemyName + " but its a Enemy the drive forword: ");
 		badEnemy.flyUp();
 	}
 
 	@Override
 	public void driveBackward() {
-		System.out.print(
-				"This may be " + badEnemy.getClass().getSimpleName() + " but its a Enemy that drive backward: ");
+		System.out
+				.print("This may be " + badEnemyName + " but its a Enemy that drive backward: ");
 		badEnemy.flyDown();
 	}
 
 	@Override
 	public void fireWeapon() {
-		System.out.print(
-				"This may be " + badEnemy.getClass().getSimpleName() + " but its a Enemy that fire a weapon: ");
+		System.out
+				.print("This may be " + badEnemyName + " but its a Enemy that fire a weapon: ");
 		badEnemy.launchesMissile();
 	}
 
